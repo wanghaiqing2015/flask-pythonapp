8cc20269bec84c9bbea0d0d4943edf323300fab5
Wang Haiqing <krman@163.com>
2020-10-16 14:25:33 +0800

    111

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     
-    ip = os.environ.get('ip','没有找到ip地址')
+    ip = os.environ.get('ip','127.0.0.1')
     
     return "ip: {}".format(ip)
 
