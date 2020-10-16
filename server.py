52737cfee3e877d528e60a28f5f4e55698a03406
Wang Haiqing <krman@163.com>
2020-10-16 14:34:13 +0800

    111

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 
 import os
+import requests
 from flask import Flask
 
 
@@ -10,7 +11,13 @@ def hello():
     
     ip = os.environ.get('ip','127.0.0.1')
     
-    return "ip: {}".format(ip)
+    try:
+        r = requests.get('http://{}'.format(ip))
+        text=r.text
+    except Exception as e:
+        text='----------'
+ 
+    return "ip: {ip}  {text}".format(ip=ip, text=text)
 
 
 if __name__ == "__main__":
